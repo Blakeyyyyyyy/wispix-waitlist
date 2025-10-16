737fb82d1348354692927e0cd7dee661264d06fc
Blake Stephens <blakestephens@MacBook-Pro-5.local>
2025-10-16 15:41:41 +0800

    Remove TimerPage - fix TypeScript build error

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ChatProvider } from '@/context/ChatContext';
 import LandingPage from '@/pages/LandingPage';
-import TimerPage from '@/pages/TimerPage';
 
 const App: React.FC = () => {
   return (
@@ -10,7 +9,6 @@ const App: React.FC = () => {
       <ChatProvider>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/timer" element={<TimerPage />} />
           <Route path="/workflow" element={<div className="p-8 text-center">Workflow page coming soon...</div>} />
         </Routes>
       </ChatProvider>
